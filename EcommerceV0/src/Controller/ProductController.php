6f9f9f278e1caa3f3dd1b5e453d732bfe97d91e8
Hassan_3060 <hassanaljamal495@gmail.com>
2025-06-06 23:49:20 +0200

    Add footer to base template and update product details in views

--- EcommerceV0/src/Controller/ProductController.php
+++ EcommerceV0/src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,20 +11,27 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ProductController extends AbstractController
 {
     #[Route('/product', name: 'product.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, ProductRepository $productRepository): Response
     {
-        return $this->render('product/index.html.twig');
+        $products = $productRepository->findAll();
+        return $this->render('product/index.html.twig',
+            [
+                'products' => $products,
+            ]
+        );
     }
 
-    #[Route('/product/{item}-{size}-{id}', name: 'product.show', requirements: ['item' => '[a-z0-9-]+', 'size' => '\w+', 'id' => '\d+'])]
-    public function detail(Request $request, String $item, String $size, int $id): Response
+    #[Route('/product/{item}-{size}-{id}', name: 'product.show', requirements: ['item' => '[a-zA-Z0-9\- ]+', 'size' => '\w+', 'id' => '\d+'])]
+    public function detail(Request $request, string $item, string $size, int $id, ProductRepository $productRepository): Response
     {
+        $product = $productRepository->find($id);
+        if ($product->gettitle() != $item || $product->getSize() != $size) {
+            return $this->redirectToRoute('product.show', ['item' => $product->gettitle(), 'size' => $product->getSize(), 'id' => $product->getId()]);
+        }
 
         return $this->render('product/detail.html.twig',
             [
-                'item' => $item,
-                'size' => $size,
-                'id' => $id,
+                'product' => $product
             ]
         );
     }
